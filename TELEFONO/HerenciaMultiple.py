8b1d88b756f5c7376ada04f977ba4a8f9483eb2c
thefresh26 <thefresh334@gmail.com>
2025-11-11 21:10:52 -0500

    Telefono

--- TELEFONO/HerenciaMultiple.py
+++ TELEFONO/HerenciaMultiple.py
@@ -1,2 +1,21 @@
 
 #del se utiliza para limpiar recursos
+
+class Smartphone:
+    def __init__(self):
+        pass
+    
+    def Telefono(self):
+        print("Haciendo una llamada...")
+        
+    def Camara(self):
+        pass
+    
+    def ReproduccionVideo(self):
+        print("Reproduciendo un video...")
+
+    def __del__(self):
+        print("Telefono Apagado")
+
+ver = Smartphone()
+ver.camara()
