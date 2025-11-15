3cbf3512f4c235477e08266a547f6daa149e3a27
thefresh26 <thefresh334@gmail.com>
2025-11-15 12:28:03 -0500

    nuevo

--- PROPIEDADES/propi.py
+++ PROPIEDADES/propi.py
@@ -1,23 +1,37 @@
 #Propiedades
 
 class empleado:
-    def __init__(self, cliente, compra):
-        self.cliente = cliente
-        self.compra = compra
+    def __init__(self, cliente, ingresos):
+        self.__cliente = cliente
+        self.__ingresos = ingresos
         
+    #GET
     def __getcliente(self):
         return self.__cliente
-    def __getcompra(self):
-        return self.__compra
+    def __getingresos(self):
+        return self.__ingresos
     
-    def __setcliente(self):
-        return self.__cliente
-    def __setcompra(self):
-        return self.__compra
+    #SET
+    def __setcliente(self,cliente):
+        self.__cliente = cliente
+    def __setingresos(self,ingresos):
+        self.__ingresos = ingresos
     
+    #DEL 
     def __delcliente(self):
-        return self.__cliente
-    def __delcompra(self):
-        return self.__compra
+        del self.__cliente
+    def __delingresos(self):
+        del self.__ingresos
+
+    cliente = property(fget= __getcliente,
+                       fset= __setcliente,
+                       fdel= __delcliente,
+                       
+                       doc= f'Soy el cliente')
     
-        
+    ingreso = property(fget= __getingresos)
+    
+
+cliente = empleado('juan',2000)
+ingreso = empleado('juan',2000)
+print(ingreso.ingreso)
